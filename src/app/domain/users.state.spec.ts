de6f3a2f3b938bb43c8c8e1f80d4164a8b57075c
Greg <gregradzio@gmail.com>
2020-08-02 01:21:03 +0200

    finished the mini serie

--- src/app/domain/users.state.spec.ts
+++ src/app/domain/users.state.spec.ts
@@ -1,11 +1,100 @@
-import {UsersState} from './users.state';
+import {UsersState, UsersStateModel} from './users.state';
+import {GetsAllUsers} from './users.service';
+import {EMPTY, of} from 'rxjs';
+import {async, TestBed} from '@angular/core/testing';
+import {UserModel} from './user.model';
 
 describe('UsersState', () => {
-  it('should get all users', () => {
-    const state = new UsersState({allUsers: []});
+  let getAllUsers: GetsAllUsers;
 
-    state.allUsers$.subscribe(users =>
-      expect(users.length).toEqual(0)
+  const makeUsersStub = (count: number): UserModel[] =>
+    Array(count)
+      .map((current: number) => ({
+        login: `login${current}`,
+        avatarUrl: {
+          url: `https://image.com/image${current}.jpg`
+        }
+      }));
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        {
+          provide: 'GetsAllUsers', useValue: {
+            getAll: () => EMPTY
+          }
+        }
+      ]
+    }).compileComponents();
+
+    getAllUsers = TestBed.get('GetsAllUsers');
+  }));
+
+  const makeUserState = (usersStub: UserModel[], getsAllUsers: GetsAllUsers): UsersState =>
+    new UsersState({allUsers: usersStub, count: usersStub.length}, getsAllUsers);
+
+  it('should init state', done => {
+    const state = makeUserState(makeUsersStub(0), getAllUsers);
+    state.allUsers$
+      .subscribe(users => {
+        expect(users).toEqual([]);
+        expect(users.length).toEqual(0);
+        done();
+      }
     );
+
+    state.count$
+      .subscribe(count => {
+        expect(count).toEqual(0);
+        done();
+      });
+  });
+
+  it('should get all users', done => {
+    const usersStub: UserModel[] = makeUsersStub(3);
+    const state = makeUserState(usersStub, getAllUsers);
+    const getAllUsersSpy = spyOn(getAllUsers, 'getAll')
+      .and.returnValue(of(usersStub));
+
+    state.loadAllUsers();
+
+    expect(getAllUsersSpy).toHaveBeenCalled();
+    state.allUsers$
+      .subscribe(users => {
+        expect(users).toEqual(usersStub);
+        done();
+      }
+    );
+
+    state.count$
+      .subscribe(count => {
+        expect(count).toEqual(usersStub.length);
+        done();
+      });
+  });
+
+  it('should override all users', done => {
+    const usersStub: UserModel[] = makeUsersStub(2);
+    const state = makeUserState(usersStub, getAllUsers);
+    const newUsersStub: UserModel[] = makeUsersStub(3);
+
+    const getAllUsersSpy = spyOn(getAllUsers, 'getAll')
+      .and.returnValue(of(newUsersStub));
+
+    state.loadAllUsers();
+
+    expect(getAllUsersSpy).toHaveBeenCalled();
+    state.allUsers$
+      .subscribe(users => {
+          expect(users).toEqual(newUsersStub);
+          done();
+        }
+      );
+
+    state.count$
+      .subscribe(count => {
+        expect(count).toEqual(newUsersStub.length);
+        done();
+      });
   });
 });
--- src/app/domain/users.state.ts
+++ src/app/domain/users.state.ts
@@ -1,10 +1,38 @@
-import {Observable, of} from 'rxjs';
+import {BehaviorSubject, Observable, of, Subject} from 'rxjs';
 import {UserModel} from './user.model';
+import {GetsAllUsers} from './users.service';
+import {Inject, Injectable} from '@angular/core';
+import {map, tap} from 'rxjs/operators';
 
+export interface UsersStateModel {
+  allUsers: UserModel[];
+  count: number;
+}
+
+@Injectable()
 export class UsersState {
-  constructor(private initialState: any) {}
+  private subject: BehaviorSubject<UsersStateModel> = new BehaviorSubject({allUsers: [], count: 0});
+  constructor(
+    private initialState: UsersStateModel,
+    @Inject('GetsAllUsers') private getsAllUsers: GetsAllUsers
+  ) {
+    this.subject.next({...initialState});
+  }
+
+  loadAllUsers(): void {
+    this.getsAllUsers.getAll()
+      .pipe(
+        tap(users => this.subject.next({allUsers: [...users], count: users.length}))
+      ).subscribe();
+  }
 
   get allUsers$(): Observable<UserModel[]> {
-    return of(this.initialState.allUsers);
+    return this.subject.asObservable()
+      .pipe(map(state => state.allUsers));
+  }
+
+  get count$(): Observable<number> {
+    return this.subject.asObservable()
+      .pipe(map(state => state.count));
   }
 }
